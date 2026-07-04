26e2a9aec4630f10d50dc2bbd54cfdf26cf29f08
Chandrasekhar <33287552+cghatam@users.noreply.github.com>
2026-07-04 19:19:10 +0530

    Modify output message in HelloWorld class

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld{
     public static void main(String[] aregs){
-        System.out.println("Hello world");
+        System.out.println("Hello world modified by Chandasekhar");
     }
 }
   
